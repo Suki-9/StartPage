46cbef24bf05291b8dfb655509d5bacdf935349d
suki-9 <suki.music2004@gmail.com>
2023-08-21 07:25:34 +0900

    カレンダー生やした

---
+++ src/components/global/Calendar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const now = new Date();
+const day: number = now.getDate()
+const month: number = now.getMonth() + 1
+const year: number = now.getFullYear()
+const days = (month == 2)
+    ? ((year % 4 == 0) ? 0 : 1) - ((year % 100 == 0 && year % 400 !== 0) ? 0 : 1)  + 28
+    : (month % 2 == ((month < 8) ? 0 : 1)) ? 0 : 1 + 30
+const voids = day%7 + now.getDay() + 1
+const weekDays = ["日","月","火","水","木","金","土"]
+</script>
+
+
+<template>
+    <div :class="$style.calendarMatrix">
+        <p v-for="weekDay in weekDays">{{ weekDay }}</p>
+        <p v-for="j in voids" :key="j">{{ "" }}</p>
+        <p v-for="i in days" :class="{[$style.today]: day == i }" :key="i">{{ i }}</p>
+    </div>
+</template>
+
+<style module lang="scss">
+.calendarMatrix {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+
+    width: 14vw;
+    
+    p {
+        width: calc(100%/7);
+        text-align: right;
+    }
+    .today {
+        color: red;
+    }
+}
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -2,11 +2,13 @@
 //Vue Component
 import GoogleSearchBar from "../components/global/GoogleSearchBar.vue";
 import DigitalClock from "../components/global/DigitalClock.vue";
+import Calendar from "../components/global/Calendar.vue";
 
 </script>
 
 
 <template>
+    <Calendar />
     <DigitalClock />
     <GoogleSearchBar />
 </template>
